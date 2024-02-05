78f9f9c4fdeeb71d4467d73b912b3b99300c14ac
Roaa <roaa@Roaas-MacBook-Pro.local>
2024-02-05 02:43:06 +0100

    list changes

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,9 +2,6 @@ import axios from 'axios'
 
 const apiUrl = import.meta.env.VITE_API_URL 
 
-console.log("apiUrl: ", apiUrl)
-
-
 export default class Api {
   static ApiURL = apiUrl
 
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,68 +1,71 @@
-import { Dropdown } from 'bootstrap'
-import React, { Suspense, useEffect, useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { Dropdown } from 'bootstrap';
+import React, { Suspense, useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import ModalComponent from '../common/ModalComponent';
-import Api from '../../api/Api'
+import Api from '../../api/Api';
 function List() {
-  const navigate = useNavigate()
-  const [list, setList] = useState([])
-  const [usersList, setUsersList] = useState([])
-  const [poolList, setpoolList] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [selectedFirstLevel, setSelectedFirstLevel] = useState('')
-  const [selectedSecondLevel, setSelectedSecondLevel] = useState('')
-  const [showAll, setShowAll] = useState(false)
-  const [showPool, setShowPool] = useState(false)
+  const navigate = useNavigate();
+  const [list, setList] = useState([]);
+  const [usersList, setUsersList] = useState([]);
+  const [poolList, setpoolList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedFirstLevel, setSelectedFirstLevel] = useState('');
+  const [selectedSecondLevel, setSelectedSecondLevel] = useState('');
+  const [showAll, setShowAll] = useState(false);
+  const [showPool, setShowPool] = useState(false);
   const [show, setShow] = useState(false);
   const [selectedUser, setSelectedUser] = useState({});
-  
 
-  let userId = localStorage.getItem('userId')
-  let role = localStorage.getItem('role')
+  let userId = localStorage.getItem('userId');
+  let role = localStorage.getItem('role');
   const getList = async () => {
-    setLoading(true)
-    setList([])
-    let path = role === 'admin2' ? 'users/list' : 'users/tree'
-    const response = await Api.call({}, path, 'get', userId)
+    setLoading(true);
+    setList([]);
+    let path = role === 'admin2' ? 'users/list' : 'users/tree';
+    const response = await Api.call({}, path, 'get', userId);
     if (response.data) {
-      let list = response.data.data
+      let list = response.data.data;
 
-      setList(list)
-      setSelectedFirstLevel(list[0])
-      setSelectedSecondLevel('')
+      setList(list);
+      setSelectedFirstLevel(list[0]);
+      setSelectedSecondLevel('');
       if (role === 'master') {
-        const response = await Api.call({}, 'users/list', 'get', userId)
+        const response = await Api.call({}, 'users/list', 'get', userId);
         if (response.data) {
-          let allUsers = response.data.data
-          setUsersList(allUsers)
+          let allUsers = response.data.data;
+          setUsersList(allUsers);
         }
-        const poolresponse = await Api.call({}, 'users/pool', 'get', userId)
+        const poolresponse = await Api.call({}, 'users/pool', 'get', userId);
         if (poolresponse.data) {
-          let allUsers = poolresponse.data.data
-          setpoolList(allUsers)
+          let allUsers = poolresponse.data.data;
+          setpoolList(allUsers);
         }
-
-        
       } else if (role === 'admin1' || role === 'admin2') {
-        const response = await Api.call({}, 'users/pool', 'get', userId)
+        const response = await Api.call({}, 'users/pool', 'get', userId);
         if (response.data) {
-          let allUsers = response.data.data
-          setUsersList(allUsers)
+          let allUsers = response.data.data;
+          const responseUsers = await Api.call({}, 'users/list', 'get', userId);
+          if (responseUsers.data) {
+            let usersList = responseUsers.data.data.concat(allUsers);
+            setUsersList(usersList);
+          }
         }
       }
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
   useEffect(() => {
     if (role !== 'user') {
-      getList()
+      getList();
     }
-  }, [])
+  }, []);
 
   const renderUserTree = () => {
     if (role === 'master' && !selectedSecondLevel) {
-      const firstLevel = []
-      const secondtLevel = []
+      const firstLevel = [];
+      const secondtLevel = [];
+      let secondLevelTable = false;
+      let secondList = [];
       list.map((item) => {
         firstLevel.push(
           <button
@@ -74,46 +77,56 @@ function List() {
             id={item.id}
             onClick={() => setSelectedFirstLevel(item)}
           >{`${item.name2} ${item.name1}`}</button>
-        )
+        );
         if (item.id === selectedFirstLevel.id) {
-          item.admins2?.map((secondItem) => {
-            secondtLevel.push(
-              <button
-                className={`btn d-block btn-outline-secondary`}
-                onClick={() => setSecondLevel(secondItem)}
-              >{`${secondItem.name2} ${secondItem.name1}`}</button>
-            )
-          })
+          if (item.admins2.length === 0) {
+            secondLevelTable = true;
+            secondList = usersList.filter(
+              (user) => user.parent?.id === item.id
+            );
+          } else {
+            item.admins2?.map((secondItem) => {
+              secondtLevel.push(
+                <button
+                  className={`btn d-block btn-outline-secondary`}
+                  onClick={() => setSecondLevel(secondItem)}
+                >{`${secondItem.name2} ${secondItem.name1}`}</button>
+              );
+            });
+          }
         }
-      })
+      });
       return (
         <>
-          <div className='d-flex levels-section'>
-            <div className='first level'> {firstLevel}</div>
-            <div className='second level'> {secondtLevel}</div>
+          <div className="d-flex levels-section">
+            <div className="first level"> {firstLevel}</div>
+            <div className="second level">
+              {secondLevelTable
+                ? secondList.length !== 0 && renderTable(secondList)
+                : secondtLevel}
+            </div>
           </div>
 
-          <div className='show-all-section'>
+          <div className="show-all-section">
             <button
-              className='btn show-all'
+              className="btn show-all"
               onClick={() => setShowPool(!showPool)}
             >{`${showPool ? 'Hide Users Pool' : 'Show Users Pool'}`}</button>
           </div>
           {showPool && renderTable(poolList)}
 
-
-          <div className='show-all-section'>
+          <div className="show-all-section">
             <button
-              className='btn show-all'
+              className="btn show-all"
               onClick={() => setShowAll(!showAll)}
             >{`${showAll ? 'Hide All Users' : 'Show All Users'}`}</button>
           </div>
           {showAll && renderTable(usersList)}
         </>
-      )
+      );
     } else if (role === 'admin1' && !selectedSecondLevel) {
-      const firstLevel = []
-      let secondtLevel = []
+      const firstLevel = [];
+      let secondtLevel = [];
       list.map((item) => {
         firstLevel.push(
           <button
@@ -124,84 +137,87 @@ function List() {
             }`}
             onClick={() => setSelectedFirstLevel(item)}
           >{`${item.name2} ${item.name1}`}</button>
-        )
+        );
         if (item.id === selectedFirstLevel.id) {
-          secondtLevel = item.admins2
+          let extraUsers = usersList.filter(
+            (user) => user.parent?.id === item.id
+          );
+          secondtLevel = extraUsers;
         }
-      })
+      });
       return (
         <>
-          <div className='d-flex levels-section-table'>
-            <div className='first level'> {firstLevel}</div>
-            <div className='second level'>
+          <div className="d-flex levels-section-table">
+            <div className="first level"> {firstLevel}</div>
+            <div className="second level">
               {' '}
               {renderTable(secondtLevel, true)}
             </div>
           </div>
-          <div className='show-all-section'>
+          <div className="show-all-section">
             <button
-              className='btn show-all'
+              className="btn show-all"
               onClick={() => setShowAll(!showAll)}
             >{`${showAll ? 'Hide All Users' : 'Show Users Pool'}`}</button>
           </div>
           {showAll && renderTable(usersList)}
         </>
-      )
+      );
     } else {
       return (
         <>
           {renderTable()}
-          <div className='show-all-section'>
+          <div className="show-all-section">
             <button
-              className='btn show-all'
+              className="btn show-all"
               onClick={() => setShowAll(!showAll)}
             >{`${showAll ? 'Hide All Users' : 'Show Users Pool'}`}</button>
           </div>
           {showAll && renderTable(usersList)}
         </>
-      )
+      );
     }
-  }
+  };
 
   const setSecondLevel = async (item) => {
-    setLoading(true)
-    let path = `users/list`
-    const response = await Api.call({}, path, 'get', item.id)
+    setLoading(true);
+    let path = `users/list`;
+    const response = await Api.call({}, path, 'get', item.id);
     if (response.data) {
-      let list = response.data.data
-      setList(list)
-      setSelectedSecondLevel(item)
-      setLoading(false)
+      let list = response.data.data;
+      setList(list);
+      setSelectedSecondLevel(item);
+      setLoading(false);
     }
-  }
+  };
 
   const userUpdate = (user) => {
-    localStorage.setItem('userProfileId', user.id)
-    localStorage.setItem('userProfileRole', user.role)
-    navigate('/setting/user-info')
-  }
+    localStorage.setItem('userProfileId', user.id);
+    localStorage.setItem('userProfileRole', user.role);
+    navigate('/setting/user-info');
+  };
 
   const showModal = (user) => {
-    setSelectedUser(user)
-    setShow(true)
-  }
+    setSelectedUser(user);
+    setShow(true);
+  };
 
   const renderTable = (userList = [], showExtra = false) => {
-    const listToRender = userList.length === 0 ? list : userList
+    const listToRender = userList.length === 0 ? list : userList;
     return (
-      <div class='table-responsive'>
-        <table className='table'>
+      <div class="table-responsive">
+        <table className="table">
           <thead>
             <tr>
-              <th scope='col'>筐体番号</th>
-              <th scope='col'>姓</th>
-              <th scope='col'>名</th>
-              <th scope='col'>ニックネーム</th>
-              <th scope='col'>Email</th>
-              <th scope='col'>属性</th>
-              <th scope='col'>状態</th>
-              {showExtra && <th scope='col'>Show</th>}
-              <th scope='col'>F</th>
+              <th scope="col">筐体番号</th>
+              <th scope="col">姓</th>
+              <th scope="col">名</th>
+              <th scope="col">ニックネーム</th>
+              <th scope="col">Email</th>
+              <th scope="col">属性</th>
+              <th scope="col">状態</th>
+              {showExtra && <th scope="col">Show</th>}
+              <th scope="col">F</th>
             </tr>
           </thead>
           {!loading && (
@@ -218,57 +234,63 @@ function List() {
                     <td>{'有効'}</td>
                     {showExtra && (
                       <td>
-                        <button
-                          className='btn btn-outline-secondary btn-sm mx-2'
-                          onClick={() => setSecondLevel(user)}
-                        >
-                          Show children
-                        </button>
+                        {user.role !== 'user' && (
+                          <button
+                            className="btn btn-outline-secondary btn-sm mx-2"
+                            onClick={() => setSecondLevel(user)}
+                          >
+                            Show children
+                          </button>
+                        )}
                       </td>
                     )}
                     <td>
                       <button
-                        className='btn btn-primary btn-sm mx-2'
+                        className="btn btn-primary btn-sm mx-2"
                         onClick={() => userUpdate(user)}
                       >
                         編集
                       </button>
-                      <button className='btn btn-danger btn-sm '
-                       onClick={() => showModal(user)}>削除</button>
+                      <button
+                        className="btn btn-danger btn-sm "
+                        onClick={() => showModal(user)}
+                      >
+                        削除
+                      </button>
                     </td>
                   </tr>
-                )
+                );
               })}
             </tbody>
           )}
         </table>
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <div className='list-page m-4'>
-      <div className='back-section'>
+    <div className="list-page m-4">
+      <div className="back-section">
         {selectedSecondLevel && (
           <button
-            className='btn btn-outline-primary clear-btn px-4'
+            className="btn btn-outline-primary clear-btn px-4"
             onClick={() => {
-              getList()
+              getList();
             }}
           >
             Clear Filter
           </button>
         )}
         <button
-          className='btn btn-outline-primary back-btn px-4'
+          className="btn btn-outline-primary back-btn px-4"
           onClick={() => navigate('/setting')}
         >
           戻る
         </button>
       </div>
       {loading ? (
-        <div class='spinner-border text-secondary' role='status'>
-          <span class='visually-hidden'>Loading...</span>
+        <div class="spinner-border text-secondary" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
       ) : role === 'admin2' ? (
         renderUserTree()
@@ -284,7 +306,7 @@ function List() {
         />
       )}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
